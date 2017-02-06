8ef04ecc7d2efa7943ef70ed1aa0cd5ef0e3dac1
Paweł Sierszeń <pawelsierszen@gmail.com>
2017-02-06 04:17:42 +0100

    Use ColorThief

--- main.py
+++ main.py
@@ -1,45 +1,41 @@
 from __future__ import division
 import utils
-import csv
+import os
+import sys
 
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
-figure = plt.figure()
-axes = figure.add_subplot(111, projection='3d')
+from colorthief import ColorThief
 
 
-def rgb_string_to_denormalized_hls(row):
-    rgb = row[0][1:]
-    r = int(rgb[0] + rgb[1], 16)
-    g = int(rgb[2] + rgb[3], 16)
-    b = int(rgb[4] + rgb[5], 16)
-    hls = utils.rgb_to_hls(r, g, b)
-    return hls[0] * 360, hls[1] * 100, hls[2] * 100
+def pick_color(filename):
+    color_thief = ColorThief(filename)
+    return color_thief.get_color(quality=1)
 
 
-def processFile(filename, **kwargs):
-    with open(filename, 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            try:
-                hls = rgb_string_to_denormalized_hls(row)
-            except IndexError:
-                continue
+if __name__ == '__main__':
+    dir1 = sys.argv[1]
+    dir2 = sys.argv[2]
+    figure = plt.figure()
+    axes = figure.add_subplot(111, projection='3d')
 
-            # print "%s\t%s\t%s" % (hls[0], hls[1], hls[2])
-            axes.scatter(hls[0], hls[1], hls[2], **kwargs)
+    for dir, color in [(dir1, 'r'), (dir2, 'b')]:
+        progress = 0
+        for filename in os.listdir(dir):
+            if progress % 10 == 0:
+                print progress
+            progress += 1
+            rgb = pick_color(os.path.join(dir, filename))
+            hls = utils.rgb_to_hls(*rgb)
+            axes.scatter(hls[0], hls[1], hls[2], c=color)
 
+    axes.set_xlabel('Hue')
+    axes.set_xlim3d(0, 360)
+    axes.set_ylabel('Lightness')
+    axes.set_ylim3d(0, 100)
+    axes.set_zlabel('Saturation')
+    axes.set_zlim3d(0, 100)
 
-processFile('colours/azzurro.csv', c='r')
-processFile('colours/lazurowy.csv', c='b')
-
-axes.set_xlabel('Hue')
-axes.set_xlim3d(0, 360)
-axes.set_ylabel('Lightness')
-axes.set_ylim3d(0, 100)
-axes.set_zlabel('Saturation')
-axes.set_zlim3d(0, 100)
-
-plt.show()
+    plt.show()
--- utils.py
+++ utils.py
@@ -2,8 +2,9 @@ import colorsys
 
 
 def rgb_to_hls(r, g, b):
-    """Takes rgb 0-255 values and returns hls 0-1 values"""
+    """Takes rgb 0-255 values and returns hls 0-360, 0-100, 0-100 values"""
     assert 0 <= r <= 255
     assert 0 <= g <= 255
     assert 0 <= b <= 255
-    return colorsys.rgb_to_hls(r / 255.0, g / 255.0, b / 255.0)
+    hls = colorsys.rgb_to_hls(r / 255.0, g / 255.0, b / 255.0)
+    return hls[0] * 360, hls[1] * 100, hls[2] * 100
